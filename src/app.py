576add8f4bdae187e6e6018cfe483120d4680b5f
Markel Kaluhin <markel.kaluhin@gmail.com>
2023-10-02 23:16:56 -0400

    Linters had been applied to the `app.py` file

--- src/app.py
+++ src/app.py
@@ -12,7 +12,5 @@ def example(number_a: int | float, number_b: int | float) -> float:
     if isinstance(number_a, (int, float)) and isinstance(number_b, (int, float)):
         result = float(number_a + number_b)
     else:
-        raise TypeError(
-            "At least one type isn't integer or float, enter valid values to sum the arguments"
-        )
+        raise TypeError("At least one type isn't integer or float, enter valid values to sum the arguments")
     return result
